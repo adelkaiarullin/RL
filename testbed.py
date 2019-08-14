241261882600a6524b8cb440a9c1db809f2a70ea
adel <yarullin91@inbox.ru>
2019-08-14 12:24:00 +0300

    init the repo

--- testbed.py
+++ testbed.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     steps = 10000
     eps = 0.1
     alpha = 0.1
-    num_bandits = 5
+    num_bandits = 10
 
     bandits = np.random.rand(num_bandits)
     estimates = np.zeros(num_bandits, dtype=np.float32)
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     counter = 0
     best_choice = 0
     acc = 0
+    estimate_history = [[] for _ in range(num_bandits)]
+    acc_history = []
 
     while counter < steps:
         counter += 1
@@ -29,11 +31,20 @@ if __name__ == '__main__':
         estimates[best_choice] *= 1 - alpha
         estimates[best_choice] += alpha * np.random.normal(loc = bandits[best_choice])
 
-        if counter % 500 == 0:
-            print('Estimates {} '.format(estimates))
-            print('Accuracy {} '.format(acc/counter))
+        acc_history.append(acc/counter)
+        estimate_history[best_choice].append(estimates[best_choice])
+
     
+    print('Estimates {} '.format(estimates))
     print('True value {}'.format(bandits))
+    #fig, axs = plt.subplots(nrows=1, ncols=2, sharex=True)
+    fig, axs = plt.subplots()
+    axs.set_title('Distributions of estimates')
+    axs.boxplot(estimate_history)
+    fig, axs = plt.subplots()
+    axs.set_title('Percent of right choice')
+    axs.plot(range(len(acc_history)), acc_history)
+    plt.show()
     
     
 
