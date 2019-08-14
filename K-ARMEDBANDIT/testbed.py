5709f369549763b8c8f8072c1edd6deb3cca5cee
adel <yarullin91@inbox.ru>
2019-08-14 14:59:03 +0300

    added nonstationary distribution

--- K-ARMEDBANDIT/testbed.py
+++ K-ARMEDBANDIT/testbed.py
@@ -1,21 +1,34 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    steps = 10000
+    if len(sys.argv) != 2:
+        print('Incorrect arguments')
+        exit()
+
+    state = sys.argv[1]
+    if state == 'stationary':
+        stationary = True
+    else:
+        stationary = False
+
+    steps = 100000
     eps = 0.1
     alpha = 0.1
     num_bandits = 10
 
-    bandits = np.random.rand(num_bandits)
+    bandit_means = np.random.rand(num_bandits)
     estimates = np.zeros(num_bandits, dtype=np.float32)
-    max_est = estimates.argmax()
+    std = 1.0
+    std_step = 1e-3
+    max_est = bandit_means.argmax()
     
     counter = 0
     best_choice = 0
     acc = 0
-    estimate_history = [[] for _ in range(num_bandits)]
+    estimate_history = [[0.0] for _ in range(num_bandits)]
     acc_history = []
 
     while counter < steps:
@@ -28,21 +41,27 @@ if __name__ == '__main__':
             best_choice = estimates.argmax()
         if max_est == best_choice:
             acc += 1
-        estimates[best_choice] *= 1 - alpha
-        estimates[best_choice] += alpha * np.random.normal(loc = bandits[best_choice])
+
+        if stationary is True:
+            R = np.random.normal(loc = bandit_means[best_choice])
+            estimates[best_choice] += (R - estimates[best_choice]) / (len(estimate_history[best_choice]))
+        else:
+            estimates[best_choice] *= 1 - alpha
+            estimates[best_choice] += alpha * np.random.normal(loc = bandit_means[best_choice], scale=std)
+            std += std_step
 
         acc_history.append(acc/counter)
         estimate_history[best_choice].append(estimates[best_choice])
 
     
     print('Estimates {} '.format(estimates))
-    print('True value {}'.format(bandits))
-    #fig, axs = plt.subplots(nrows=1, ncols=2, sharex=True)
+    print('True value {}'.format(bandit_means))
+
     fig, axs = plt.subplots()
     axs.set_title('Distributions of estimates')
     axs.boxplot(estimate_history)
     fig, axs = plt.subplots()
-    axs.set_title('Percent of right choice')
+    axs.set_title('Percent of right choice where eps is {}'.format(eps))
     axs.plot(range(len(acc_history)), acc_history)
     plt.show()
     
